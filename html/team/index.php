c5de077f681e68700383d7c1b328e9377290fffd
coinop-logan <coinop.logan@gmail.com>
2020-03-02 15:45:31 +0200

    Add some team info

---
+++ html/team/index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/common-assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/common-assets/css/style.css">
+    <script src="/common-assets/js/jquery-3.4.1.min.js"></script>
+    <script src="/common-assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+        // ini_set('display_errors', 1);
+        // ini_set('display_startup_errors', 1);
+        // error_reporting(E_ALL);
+        include($_SERVER['DOCUMENT_ROOT'] . "/common-assets/html/header.html");
+    ?>
+    <div align=center class="body">
+        <div align=left style="max-width: 1000px;">
+            <h1>Team Toast</h1>
+            <div class="text-block container-fluid">
+                <p>Team Toast is a small team centered around the development of DAIHard and Foundry.</p>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img class="img-fluid" style="width: 100%;" src="/common-assets/img/oglog.jpg">
+                    </div>
+                    <div class="col-sm-9">
+                        <h2 style="font-weight:700">OG Log</h2>
+                        <p>Co-founder and CEO of Team Toast. Lead developer for DAIHard and Foundry.
+                            Developed the Burnable Payment and DAIHard smart contracts.
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3">PIC</div>
+                    <div class="col-sm-9">
+                        <h2 style="font-weight:700">The Schalkinator</h2>
+                        <p>Co-founder and Sugar Daddy of Team Toast. Developed the Bucket Sale smart contract and tests.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

a5944a10a56204b95ad69da44dc8431033b172ce
coinop-logan <coinop.logan@gmail.com>
2020-04-30 13:26:15 +0200

    Start redesign... Fuck elmstatic...

--- html/index.php
+++ html/index.php
@@ -12,161 +12,22 @@
     <link rel="stylesheet" href="/common-assets/css/style.css">
     <script src="/common-assets/js/jquery-3.4.1.min.js"></script>
     <script src="/common-assets/js/bootstrap.min.js"></script>
-    <script src="/common-assets/js/elm-generated-sale-button.js"></script>
-    <script src="/common-assets/js/attach-elm-button.js"></script>
-    <script>
-        window.addEventListener("load", function(e) {
-            attachElmSaleButtons(document.querySelectorAll(".sale-button"), 1584100922);
-        });
-    </script>
 </head>
 <body>
     <?php
+        
         include($_SERVER['DOCUMENT_ROOT'] . "/common-assets/html/header.php");
         makeHeader("home");
     ?>
     <div align=center class="body">
         <div align=left style="max-width: 1000px;">
-            <div class="text-block">
-            <h1>Foundry: A DAO for Economic Freedom</h1>
-                <p>
-                    It's time for crypto to start making moves on the global stage.
-                    Foundry will be our gladiator.
-                </p>
-            <h1 class="anchor" id="foundry">FRY and Foundry</h1>
-                <p>
+            <div class="float-md-left front-page-block" style = "background-color: rgb(16, 7, 234);">
+                <div class="pretitle">THE DAO</div>
+                <div class="title">Foundry and FRY</div>
+                <div class="blurb">
                     Foundry's future is in the hands of FRY holders.
-                </p><p>
-                    Testing button:<br>
-                    <div style="height:40px">
-                    <div style="min-height:inherit" class="sale-button"></div>
-                    </div>
-                </p><p>
-                    More on Foundry -->
-                </p>
-                <h2>FRY Sale</h2>
-                    <p>
-                        The FRY Sale will mint and auction off 50,000 FRY every 7 hours
-                        until a total of 60,000,000 FRY has been disbursed.
-                        This will take just under 1 year.
-                        <a href="/sale/">More info on the FRY sale</a>
-                    </p>
-                <h2>Liquid Democracy</h2>
-                    <p>
-                        Team Toast is designing a Liquid Democracy system
-                        that allows Foundry to rely on FRY holders for intelligent decision-making,
-                        while avoiding common pitfalls of DAO governance.
-                        <a href="#">Read the Medium post LINK NEEDED</a>
-                    </p>
-                <h2>Tools for Economic Freedom</h2>
-                    <p>
-                        Foundry will accumulate FRY holders who believe in unstoppable economic freedom.
-                        After DAIHard succeeds or fails, FRY holders will likely drive Foundry
-                        to build or invest in similar pursuits: profitable tools that increase economic freedom which cannot be shut down.
-                    </p>
-            <h1>How it (might) work</h1>
-                <p>Here's a speculative, non-final schematic of how Foundry might be put together.
-                </p>
-                <img src="/common-assets/img/foundry-schematic.png" style="width:100%">
-            <h1 class="anchor" id="daihard">DAIHard: Foundry's Flagship Product</h1>
-                <p>The Unkillable, Global Gateway Between DAI and Any Other Currency</p>
-                <p>Visit the DAIHard Marketplace --></p>
-                <h2>Unkillable, Global Gateway</h2>
-                    <p>
-                        DAIHard extends many of the promises of crypto
-                        (borderless, anonymous, limitless, unstoppable) into the exchange mechanism itself,
-                        allowing <b>anyone, anywhere</b> to bypass centralized exchanges and the control they impose.
-                        See our
-                        <a href="https://www.reddit.com/r/ethereum/comments/chl924/relaunching_the_borderless_unkillable_cryptofiat/">launch post</a>
-                        for more on DAIHard and the problems it solves.
-                    </p>
-                <h2>Burnable Payments</h2>
-                    <p>
-                        DAIHard is built entirely on Ethereum smart contracts,
-                        and involves no third parties to operate (not even escrow agents).
-                        This is achieved chiefly via the game theory of Burnable Payments, which we’ve
-                        <a href="https://medium.com/@coinop.logan/daihard-game-theory-21a456ef224e">written about here</a>.
-                    </p>
-                <h2>Next Stop: Liqudity</h2>
-                    <p>
-                        <a href="daihard.exchange">The app is live on mainnet</a>,
-                        so the next step is to cut its teeth with real users while ensuring the exchange has liquidity.
-                        Thus, marketing DAIHard will be one of the main uses of the funding gained via the <a href="/sale/">FRY sale</a>.
-                    </p>
-            
-            <h1 class="anchor" id="zimdai">ZimDai: An Economic Jailbreak</h1>
-                <p>Zimbabweans are trapped in an abusive economic system. But crypto is growing teeth.</p>
-                <p>More on ZimDai --></p>
-                <p>(quote) Zimbabwe suffers under incredibly irresponsible and abusive economic control.
-                        The cryptocurrency industry and stack is now mature enough to
-                        break the Zimbabwean citizens out of this economic prison.
-                <h2>Zimbabwe's Plight</h2>
-                    <p>
-                        The official currency of Zimbabwe is rapidly inflating, and
-                        <a href="https://en.wikipedia.org/wiki/Hyperinflation_in_Zimbabwe">this is nothing new.</a>
-                        It is practically impossible to save money in such an environment,
-                        and entrepreneurship is being strangled.
-                        Worse, the banking system significantly limits withdrawals, and ATMs simply don't work.
-                        Internal political pressure has proven incapable of changing any of this.
-                    </p>
-                <h2>Blueprint For an Economic Jailbreak</h2>
-                    <p>
-                        <a href="https://github.com/coinop-logan/ZimDai/raw/master/whitepaper.pdf">The ZimDai Paper: A Blueprint for an Economic Jailbreak</a>
-                        is a thorough investigation into the viability of boostrapping Dai adoption in Zimbabwe,
-                        given the significant challenges any such endeavor faces today.
-                        It assumes a hostile state and does not ask for permission.
-                        It is the result of two months of on-the-ground research by Logan, funded by Team Toast.
-                    </p>
-                <h2>The Missing Piece</h2>
-                    <p>
-                        What's needed is a marketing drive. With both marketing leadership and funding,
-                        we may have on our hands a strikingly realistic, immediately applicable plan
-                        to liberate the Zimbabwean citizenry from systemic financial abuse.
-                        This has been a dream within crypto for years.
-                        Given significant funding, we expect Foundry to lead and fund this campaign.
-                    </p>
-            <h1 class="anchor" id="teamtoast">Team Toast</h1>
-                <div class="text-block container-fluid">
-                    <div class="row" style="padding:1em">
-                        <div class="col-sm-3">
-                            <img class="img-fluid" style="width: 100%;" src="/common-assets/img/oglog.jpg">
-                        </div>
-                        <div class="col-sm-9">
-                            <h2 style="font-weight:700">Logan Brutsche</h2>
-                            <p>Co-founder and CEO of Team Toast, and doting mother of Foundry.
-                                Lead developer for DAIHard and Foundry.
-                                Developed the Burnable Payment and DAIHard smart contracts.
-                                Uses the Cu Chi Tunnels extensively to evade The Man.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:1em">
-                        <div class="col-sm-3">
-                            <img class="img-fluid" style="width:100%;" src="/common-assets/img/schalk.png">
-                        </div>
-                        <div class="col-sm-9">
-                            <h2 style="font-weight:700">Schalk Dormehl</h2>
-                            <p>Co-founder of Team Toast and Sugar Daddy for Foundry.
-                                Pays Foundry's bills while it grows up to be a big strong DAO.
-                                Developed the Bucket Sale contract and associated tests.
-                                Leads research and development for DAO governance.
-                                Exists in a permanent state of bewildered or concerned confusion.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:1em">
-                        <div class="col-sm-3">
-                            <img class="img-fluid" style="width:100%;" src="<?php echo getRandomVPicPubSrc();?>">
-                        </div>
-                        <div class="col-sm-9">
-                            <h2 style="font-weight:700">V for Visuals</h2>
-                            <p>Visual Designer for DAIHard and Foundry. Shrouded in secrecy.
-                                Those who speak its true name invite disaster and misfortune.
-                                Coaxed into manifestation by offers of cheap liquor and forbidden dark magicks.
-                            </p>
-                        </div>
-                    </div>
                 </div>
+            </div>
         </div>
     </div>
 </body>

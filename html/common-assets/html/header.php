bb6292f9c9b9f5ac52936a63ed65d097442e0f87
coinop-logan <coinop.logan@gmail.com>
2020-05-04 09:24:46 +0200

    Partway through

--- html/common-assets/html/header.php
+++ html/common-assets/html/header.php
@@ -12,32 +12,45 @@ function makeHeader($activePageName) {
     ?>
 <script src="/common-assets/js/capture-and-hide-referral.js"></script>
 <iframe style="display:none" id="referralCaptureIframe" src="https:sale.foundrydao.com/captureReferral.html" onload="captureAndHideReferral()"></iframe> 
-<div class="jumbotron text-center" style="background-color:#071B5C; margin-bottom:0">
-    <h1 style="color:#FF006E; font-weight:900">Foundry</h1>
-    <p style="color:#FFFFFF">A DAO for Financial Freedom</p>
+<div class="my-navbar">
+    <a class="homelogo" href="/">
+        <img class="fry-logo" src="/common-assets/img/fry-icon.svg">
+        <div class="foundry">Foundry.</div>
+    </a>
+    <a class="my-navlink" href="/#team">
+        <div class="my-navlink-label">Team Toast</div>
+    </a>
+    <a class="my-navlink" href="/#zimdai">
+        <div class="my-navlink-label">ZimDai</div>
+    </a>
+    <a class="my-navlink" href="/#products">
+    <div class="my-navlink-label">Products</div>
+    </a>
+    <a class="my-navlink" href="/faq/">
+        <div class="my-navlink-label">FAQ</div>
+    </a>
+    <!-- <div class="mr-auto">
+        <li class="<?php echo maybeActive($activePageName, "home") ?>">
+            <a href="/">Home</a>
+        </li>
+        <li class="<?php echo maybeActive($activePageName, "faq") ?>">
+            <a href="/faq/">Foundry FAQ</a>
+        </li>
+        <li class="<?php echo maybeActive($activePageName, "daihard") ?>">
+            <a href="/#products">Products</a>
+        </li>
+        <li class="<?php echo maybeActive($activePageName, "zimdai") ?>">
+            <a href="/#zimdai">ZimDai</a>
+        </li>
+        <li class="<?php echo maybeActive($activePageName, "team") ?>">
+            <a href="/#team">Team Toast</a>
+        </li>
+    </div> -->
 </div>
-<nav id="navbar" class="navbar navbar-default">
-    <div class="container-fluid">
-        <ul class="nav navbar-nav">
-            <li class="nav-item <?php echo maybeActive($activePageName, "home") ?>">
-                <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item <?php echo maybeActive($activePageName, "faq") ?>">
-                <a class="nav-link" href="/faq/">Foundry FAQ</a>
-            </li>
-            <li class="nav-item <?php echo maybeActive($activePageName, "daihard") ?>">
-                <a class="nav-link" href="/#products">Products</a>
-            </li>
-            <li class="nav-item <?php echo maybeActive($activePageName, "zimdai") ?>">
-                <a class="nav-link" href="/#zimdai">ZimDai</a>
-            </li>
-            <li class="nav-item <?php echo maybeActive($activePageName, "team") ?>">
-                <a class="nav-link" href="/#team">Team Toast</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-<script src="/common-assets/js/implement-navbar-stickiness.js"></script>
+
     <?php
 }
 ?>
+
+
+

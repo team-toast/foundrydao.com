f4fdffaf0deff58671eac9c30959623c313e92a9
Wazeem Raja <wazeem@teronext.com>
2020-09-28 19:39:24 +0530

    New blog pages and roadmap added

---
+++ html/sale/indexBkup.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0094/2740.js" async="async"></script>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/common-assets/html/google-analytics-stuff.php"); ?>
+    <script src="/js/capture-and-hide-referral.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=11,chrome=1">
+    <meta name="theme-color" content="#262f68">
+    <meta name="description" content="Foundry: A DAO for Economic Freedom. Tools for Economic Freedom Foundry will accumulate FRY holders who believe in unstoppable economic freedom.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.min.css">
+    <link rel="icon" type="image/png" href="/favicon.ico"/>
+    <link rel="apple-touch-icon" href="/favicon.ico">
+    <meta http-equiv="refresh" content="1; URL=https://sale.foundrydao.com" />
+    <title>Foundry Sale Redirect</title>
+</head>
+<body>
+    <?php
+        // ini_set('display_errors', 1);
+        // ini_set('display_startup_errors', 1);
+        // error_reporting(E_ALL);
+        include($_SERVER['DOCUMENT_ROOT'] . "/common-assets/html/header.php");
+        makeHeader("Sale Redirect");
+    ?>
+    <div class="body rendered-md">
+        <h1>Redirecting to sale...</h1>
+    </div>
+    <?php
+        include($_SERVER['DOCUMENT_ROOT'] . "/common-assets/html/footer.php");
+        makeFooter();
+    ?>
+</body>
+</html>
